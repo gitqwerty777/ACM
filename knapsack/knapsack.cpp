9360f3462b07bd3feef86eed739fc1f6c8ee6589
gitqwerty777 <goohcl777@gmail.com>
2017-12-22 10:37:51 +0800

    [knapsack] try to speed up

--- knapsack/knapsack.cpp
+++ knapsack/knapsack.cpp
@@ -39,23 +39,12 @@ bool cmp(Item a, Item b)
 // an upper bound on maximum profit.
 int bound(Node& u)
 {
-  // if weight overcomes the knapsack capacity, return
-  // 0 as expected bound
-  if (u.weight >= W)
-    return 0;
-
-  // initialize bound on profit by current profit
   int profit_bound = u.profit;
-
-  // start including items from index 1 more to current
-  // item index
-  //int j = (u.boundIndex > 0)?u.boundIndex:u.level+1;//if u.boundIndex exist, directly use it
+  int totweight = u.weight;
   int j = u.level+1;
   //because u.boundIndex <= v.boundIndex;
-  int totweight = u.weight;
 
-  // checking index condition and knapsack capacity
-  // condition
+  //TODO: log(n), should remove
   int index = std::upper_bound(weightSum+j, weightSum+N, W-u.weight+weightSum[j]) - (weightSum);
   if(index == N){
     totweight += weightSum[N-1] - weightSum[u.level];
@@ -76,26 +65,18 @@ int bound(Node& u)
   {
     totweight += arr[j].weight;
     profit_bound += arr[j].value;
-    //if(profit_bound > maxProfit)
-//      return profit_bound;
     j++;
   }
-  if(j != index){
-    printf("index %d j %d, ulevel %d\n", index, j, u.level);
-    printf("find weightsum lower bound %d = %d\n", W-u.weight+weightSum[u.level], index);
-    for(int i = u.level; i < N; i++){
-      printf("weight[%d]=%lld\n", i, weightSum[i]);
-    }
-    printf("=================\n");
-    }
   */
 
-  // If k is not n, include last item partially for
-  // upper bound on profit
+  //include last item partially to upper bound
   if (j < N)
     profit_bound += (W - totweight) * arr[j].value /
       arr[j].weight;
   return profit_bound;
+}
+int bound2(Node& u){
+  return bound(u);
 }
 
 // Returns maximum profit we can get with capacity W
@@ -128,6 +109,7 @@ int knapsack()
       continue;
     else
       v.level = u.level + 1;
+
     //choose current level's item
     v.weight = u.weight + arr[v.level].weight;
     v.profit = u.profit + arr[v.level].value;
@@ -139,25 +121,21 @@ int knapsack()
         maxProfit = v.profit;
         Q.push(v);
       } else {
-// Get the upper bound on profit to decide
+        // Get the upper bound on profit to decide
         // whether to add v to Q or not.
         v.bound = bound(v);
         //printf("u boundindex %d, v boundindex %d\n", u.boundIndex, v.boundIndex);
-        // If bound value is greater than profit,
-        // then only push into queue for further
-        // consideration
         if (v.bound > maxProfit)
           Q.push(v);
       }
     }
 
-    // Do the same thing, but Without taking
-    // the item in knapsack
+    //not choose current level's item
     v.weight = u.weight;
     v.profit = u.profit;
-    v.bound = bound(v);
-    printf("u boundindex %d, v boundindex %d\n", u.boundIndex, v.boundIndex);
-    assert(u.boundIndex <= v.boundIndex);
+    v.bound = bound2(v);
+    //printf("u boundindex %d, v boundindex %d\n", u.boundIndex, v.boundIndex);
+    //assert(u.boundIndex <= v.boundIndex);
     if (v.bound > maxProfit)
       Q.push(v);
   }
@@ -166,14 +144,10 @@ int knapsack()
 
 /*
 背包大小≤ 5×106，和物品個數  ≤1000，下一行開始每行包含兩個正整數，分別代表物品價值 ≤105，和物品重量 ≤105
- */
-
-// driver program to test above function
+*/
 int main()
 {
-
   scanf("%d %d", &W, &N);
-
   int value, weight;
   for(int i = 0; i < N; i++){
     scanf("%d %d", &value, &weight);
